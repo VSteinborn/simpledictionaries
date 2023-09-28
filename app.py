a70848c8aded335f73ff19d4b6cb12a606e60f37
VSteinborn <25616342+VSteinborn@users.noreply.github.com>
2023-09-28 16:08:47 +0200

    add 404 support

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.errorhandler(404)
+def not_found(e):
+  return render_template("404.html"), 404
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
