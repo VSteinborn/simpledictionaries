af2862dbc4b15b04e111f22117c9a05acdb370f9
VSteinborn <25616342+VSteinborn@users.noreply.github.com>
2023-11-07 19:15:03 +0100

    Update formatting - black + prettier

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 export FLASK_APP=app.py
 alias setuppy="source ./env/bin/activate"
-alias setupdb="flask db init"
-alias dbmigrate='flask db migrate -m "update goods and prices"'
-alias dbupgrade='flask db upgrade'
+# alias setupdb="flask db init"
+# alias dbmigrate='flask db migrate -m "update database"'
+# alias dbupgrade='flask db upgrade'
--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 from flask import render_template, redirect, url_for
 
 from dictsite import app
-from dictsite.forms import SearchWord 
+from dictsite.forms import SearchWord
 from dictsite.fetch import get_definition
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route("/", methods=["POST", "GET"])
 def home():
     form = SearchWord()
 
     if form.validate_on_submit():
         word = form.word.data
         definition = get_definition(word)
-        return render_template('home.html', word=word, definition = definition, form=form)
+        return render_template("home.html", word=word, definition=definition, form=form)
 
-    return render_template('home.html', form=form)
+    return render_template("home.html", form=form)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
+
 
 @app.errorhandler(404)
 def not_found(e):
-  return render_template("404.html"), 404
+    return render_template("404.html"), 404
+
 
 if __name__ == "__main__":
     app.run()
